e0990e29cb217c5dcfcc2a1b0cd60edb67c5c890
naps-coder <caio.ramirez.albuquerque@gmail.com>
2025-02-24 12:00:10 -0300

    simple login input - nothing fancy

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from datetime import datetime
 import pytz
 
+nome_user = 'empty'
+
 gmt_sp = pytz.timezone('America/Sao_Paulo')
 time = datetime.now(gmt_sp)
 def call_time ():
     print('Horário atual: ',time.strftime("%H:%M:%S"))
 
+def login ():
+    nome_user = input('Insira nome de usuário: ')
+    senha = input('Insira sua senha: ')
+    print(r"""\ _    _      _                            _        
+| |  | |    | |                          | |       
+| |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___  
+| |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \ 
+\  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |
+ \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/ 
+                                                   
+                                                   
+ _____                  _       _                  
+|  _  |                (_)     | |                 
+| | | |_ __ _ __   ___  _ _ __ | |_                
+| | | | '__| '_ \ / _ \| | '_ \| __|               
+\ \_/ / |  | |_) | (_) | | | | | |_                
+ \___/|_|  | .__/ \___/|_|_| |_|\__|               
+           | |                                     
+           |_|                                     """)
 
-
+login()
